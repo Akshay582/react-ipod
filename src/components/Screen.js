abf65a10ece0738f2dde71cc5c5af94417b60bf0
Akshay Mittal <akshaymittal.da@gmail.com>
2020-04-15 10:31:21 +0530

    side-menu MVP

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -4,13 +4,15 @@ class Screen extends Component {
     render() {
         return (
             <div className="Screen">
-                <strong>iPod.js</strong>
-                <ul>
-                    <li id="songs" className="active">Songs</li>
-                    <li id="albums">Albums</li>
-                    <li id="artists">Artists</li>
-                    <li id="playlists">Playlists</li>
-                </ul>
+                <div className="side-menu">
+                    <strong>iPod.js</strong>
+                    <ul>
+                        <li id="songs" className="active">Songs</li>
+                        <li id="albums">Albums</li>
+                        <li id="artists">Artists</li>
+                        <li id="playlists">Playlists</li>
+                    </ul>
+                </div>
             </div>
         )
     }
